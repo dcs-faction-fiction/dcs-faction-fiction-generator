fd061f4b9e70b71843bfc4d2d29bfe29558ca745
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-04-10 23:08:42 +0200

    parkings

--- src/main/java/GenerateParkings.java
+++ src/main/java/GenerateParkings.java
@@ -1,18 +1,43 @@
 
 import java.io.IOException;
+import static java.lang.String.format;
+import java.util.List;
 
 public class GenerateParkings {
   public static void main(String[] args) throws IOException {
-    var parser = FileParser.open("CAUCASUS/airfield_parkings");
+    var maps = List.of("CAUCASUS");
 
-    Integer id;
-    while((id = parser.nextValueOfKey("airdromeId", Integer::valueOf)) != null) {
-      String parking = parser.nextValueOfKey("parking", s -> s);
-      String parkingId = parser.nextValueOfKey("parking_id", s -> s);
-      double x = parser.nextValueOfKey("x", Double::valueOf);
-      double y = parser.nextValueOfKey("y", Double::valueOf);
+    for (String map: maps) {
+      var parser = FileParser.open(map+"/airfield_parkings");
 
-      System.out.println(id + "=" + parking + "/" + parkingId + ": " + x + ",0" + y);
+      Integer id;
+      while((id = parser.nextValueOfKey("airdromeId", Integer::valueOf)) != null) {
+        String parking = parser.nextValueOfKey("parking", s -> s);
+        String parkingId = parser.nextValueOfKey("parking_id", s -> s);
+        double x = parser.nextValueOfKey("x", Double::valueOf);
+        double y = parser.nextValueOfKey("y", Double::valueOf);
+
+//        System.out.println(format(
+//          "insert into parkings (map, airdrome_id, parking, parking_id, x, y) "
+//          + "values('%s', %d, %s, %s, %f, %f);",
+//          map,
+//          id,
+//          parking,
+//          parkingId,
+//          x,
+//          y
+//          ));
+
+        System.out.println(format(
+          "addParking(%s, %d, %s, %s, %f, %f);",
+          map,
+          id,
+          parking,
+          parkingId,
+          x,
+          y
+        ));
+      }
     }
   }
 }
